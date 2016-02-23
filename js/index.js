cce26a56941d0c5be552d15c1de47d30180c45b9
Thierry <thierry.heumann@gmail.com>
2016-02-22 20:18:17 -0800

    draft3 2_22_2016

--- js/index.js
+++ js/index.js
@@ -1,52 +1,69 @@
+/* Google Map Initialization */
 function initMap() {
 var map = new google.maps.Map(document.getElementById('map'), {
 center: {lat: 37.8, lng: -122.6},
 zoom: 12
 });
 };
+/* End Google Map Initialization */
 
-/* Map Slide down / Slide up */
+/* Map Slide down */
 $(document).ready(function () {
   $('.map-button').click(function() {
-    $('.main').slideUp();
-    $('#map').slideDown("slow");
+    $('.main').animate({ top: '-600px' });
+    $('#map').animate({ top: '-600px' });
     $('.search-bar').slideUp("slow");
     $('.collapsed-search-bar').slideDown();
   });
-  $('.slide-button').click(function() {
-    $('.search-result').slideUp();
-    $('#map').slideUp("slow");
-    $('.search-bar').slideDown("slow");
-    $('.collapsed-search-bar').slideUp();
-    $('.main').slideDown();
-  });
-/* End Map Slide down / Slide up */
+/* End Map Slide down */
 
 /* Search box Submit handler */
   $('.wiki-search').submit(function(evt) {
     evt.preventDefault();
     var searchRequest = $('.search-input').val();
     console.log(searchRequest);
+    var wkQuerry = "https://en.wikipedia.org/w/api.php?action=query&titles=" + "San%20Franciso%20" + searchRequest + "&prop=revisions&rvprop=content&format=json"
+    console.log(wkQuerry)
     $('.search-result').append (searchRequest);
     $('.search-result').slideDown("slow");
     $('.search-bar').slideUp("slow");
-    $('.main').slideUp();
+    $('.main').animate({ top: '-600px' });
+    $('#map').animate({ top: '-1200px' });
     $('.collapsed-search-bar').slideDown();
-    $('#map').slideUp("slow");
+    $.ajax({
+      url: wkQuerry,
+      action: 'query',
+      dataType: 'json',
+      type: 'POST',
+      headers: { 'Api-User-Agent': 'SF Streets - https://github.com/theumann/theumann.github.io.git' },
+    });
   });
 /* End Search box Submit handler */
 
-});
+/* Main Section Reset */
+  $('.slide-button').click(function() {
+    $('.search-result').slideUp();
+    $('.main').animate({ top: '0' });
+    $('#map').animate({ top: '0' });
+    $('.search-bar').slideDown("slow");
+    $('.collapsed-search-bar').slideUp();
+  });
+/* End Main Section Reset */
 
+});
 
 /*TODO
 P1
 - Fix map (only shows when toggling dev tools)
 - Add text search results from wikipedia.
-- Add map communication with wikipedia
-- Fix layout issues and links
+- Add map communication with wikipedia.
+- Center map with precise lat/lng.
+- Fix layout issues and links (Contact, About...)
 
 P2
-- Enable other 2 search methods
+- Enable other 2 search methods.
 - Add more than just wikipedia results. Images, upoming events (Yelp?), etc...
+
+P3
+- User registration / Login.
 */
